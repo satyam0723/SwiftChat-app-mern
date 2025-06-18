174af061e602735ab3efdb742ff0d02756f9109a
Satyam Gupta <satgupta23@gmail.com>
2025-06-18 15:22:34 +0530

    fix

--- backend/config.js
+++ backend/config.js
@@ -2,6 +2,6 @@ import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
 
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 5000;
 export const JWT_KEY = process.env.JWT_KEY;
 export const MONGO_URI = process.env.MONGO_URI;
--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,10 @@ import { app } from "./app.js";
 import { PORT } from "./config.js";
 import setupSocket from "./socket.js";
 
-const port = process.env.PORT || 5000;
-
 connectDB()
   .then(() => {
     const server = app.listen(PORT, () => {
-      console.log(`server is running at http://localhost:${port}/`);
+      console.log(`server is running at http://localhost:${PORT}/`);
     });
     setupSocket(server);
     console.log("Socket.io server setup complete");
